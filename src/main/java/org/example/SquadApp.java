0398b649b221242ef4d28129cbee3077a4a324dc
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-04 08:33:46 +0300

    add methods to clear and delete a squad and all the squads

--- src/main/java/org/example/SquadApp.java
+++ src/main/java/org/example/SquadApp.java
@@ -14,11 +14,24 @@ public class SquadApp {
         if (allSquads != null){
             allSquads = list;
         }
-        squad.setId(list.size());
         list.add(squad);
+        squad.setId(list.size());
+
     }
 
     public void setSquads(List<Squad> squads) {
         this.allSquads = squads;
+    }
+
+    public  Squad findById(int squadId) {
+        return  allSquads.get(squadId);
+    }
+
+    public void clearAllSquads(){
+        allSquads.clear();
+    }
+
+    public void deleteSquad(int squadId) {
+        allSquads.remove(squadId);
     }
 }
