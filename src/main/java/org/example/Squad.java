4f1b92b94411acf143dd7e2a3e07f639eb191e96
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-04 08:32:13 +0300

    add a method to add a hero to a squad

--- src/main/java/org/example/Squad.java
+++ src/main/java/org/example/Squad.java
@@ -1,16 +1,33 @@
 package org.example;
 
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class Squad {
     private String squadName;
     private String squadImage;
+
+    private Set<String> squadHeroes = new HashSet<>();
 
     private int id;
 
     public Squad(String name, String image){
         this.squadName = name;
         this.squadImage = image;
+    }
+
+    public void addHero(Hero hero){
+        this.squadHeroes.add(hero.getHeroName());
+        hero.setSquadTeam(this);
+    }
+
+    public Set<String> getSquadHeroes() {
+        return squadHeroes;
+    }
+
+    public void setSquadHeroes(Set<String> squadHeroes) {
+        this.squadHeroes = squadHeroes;
     }
 
     public String getSquadName() {
