4ab4cc90cc80c05a350f5a55d584c7cf07e1a97d
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-04 08:24:48 +0300

    add a method to update hero details

--- src/main/java/org/example/Hero.java
+++ src/main/java/org/example/Hero.java
@@ -5,15 +5,46 @@ public class Hero {
     private String heroName;
     private String weakness;
     private String strength;
+    private String clubName;
+
+
+
     private int id;
     private int age;
+    private String heroImage;
 
-    public Hero(String heroName, String weakness, String strength, int id, int age) {
+    private Squad squadTeam;
+
+    public Hero(String heroName, String weakness, String strength, int age) {
         this.heroName = heroName;
         this.weakness = weakness;
         this.strength = strength;
-        this.id = id;
+        this.heroImage = "/images/logo.jpeg";
         this.age = age;
+    }
+
+    public String getHeroImage() {
+        return heroImage;
+    }
+
+    public void setHeroImage(String heroImage) {
+        this.heroImage = heroImage;
+    }
+
+    public String getClubName() {
+        clubName = squadTeam.getSquadName();
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+    public Squad getSquadTeam() {
+        return squadTeam;
+    }
+
+    public void setSquadTeam(Squad squadTeam) {
+        this.squadTeam = squadTeam;
     }
 
     public String getHeroName() {
@@ -53,6 +84,13 @@ public class Hero {
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void update(String name, String strength, String weakness , int age) {
+        this.heroName = name;
+        this.weakness = weakness;
+        this.strength = strength;
         this.age = age;
     }
 }
