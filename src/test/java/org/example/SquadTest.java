698b79edbd58b83fbbe7927046c79ecca801fcca
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-04 08:17:34 +0300

    add a test to confirm if a squad has a list of heroes

--- src/test/java/org/example/SquadTest.java
+++ src/test/java/org/example/SquadTest.java
@@ -15,6 +15,21 @@ class SquadTest {
         SquadApp squadApp = new SquadApp();
         squadApp.addSquad(new ArrayList<>(),squad);
 
+
         assertEquals(1,squad.getId());
+    }
+
+    @Test
+    public void checkHeroesInATeam() {
+
+        Squad squad = new Squad("","");
+        SquadApp squadApp = new SquadApp();
+        squadApp.addSquad(new ArrayList<>(),squad);
+        Hero hero = new Hero("1","","",25);
+        Hero hero2 = new Hero("","","",25);
+        squad.addHero(hero);
+        squad.addHero(hero2);
+
+        assertEquals(2,squad.getSquadHeroes().size());
     }
 }
